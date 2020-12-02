0dcf013d912d5872236071c63d2145b974f09bea
JinDai16 <48148623+JinDai16@users.noreply.github.com>
2020-12-02 00:00:10 -0600

    Update ATM.cpp

--- ATM.cpp
+++ ATM.cpp
@@ -1,11 +1,22 @@
 #include "ATM.h"
 
+//Methods for the Class Account
 Account::Account(int tp, int bal): type(ty), balance(bal) {}
+
+// Operator overloading
+Account Account:: operator = (const Account& account)
+{
+	this->type = account.type;
+	this->balance = account.balance;
+	return *this;
+}
+// function to display the current balance of an account
 void Account::getBalance()
 {
         cout << "Your account balance is: $" << this->balance << endl;   
 }
 
+// function to make a withdrawal from an account
 void Account::getWithdraw()
 {
         int withdraw_amount;
@@ -28,6 +39,7 @@ void Account::getWithdraw()
         }
 }
 
+// function to make deposit to the current account
 void Account::getDeposit()
 {
 
@@ -40,8 +52,11 @@ void Account::getDeposit()
         cout << "$" << deposit_amount << "was deposited into your account" << endl;
         cout << "Balance in the Account:" << this->balance << endl;
 }
-//==========================================================================================
+
+//Methods for the Class Customer
 Customer::Customer(string& un, string pin): username(un), PIN(pin) {}
+
+// fuction to create a new account owned by the customer
 void Customer::CreateNewAccount(int tp, int bal)
 {
         if(tp != 1 || tp != 2) //invalid account type
@@ -56,6 +71,7 @@ void Customer::CreateNewAccount(int tp, int bal)
         this->accounts.push_back(newAccount);
 }
 
+// function for the customer to make a withdrawal from a certain account
 void Customer::Withdraw(Account account)
 {
         auto it = find(this->accounts.begin(), this->accounts.end(), account);
@@ -69,6 +85,7 @@ void Customer::Withdraw(Account account)
         }
 }
 
+// function for the customer to make a deposit to a certain account
 void Customer::Depoist(Account account)
 {
         auto it = find(this->accounts.begin(), this->accounts.end(), account);
@@ -82,6 +99,7 @@ void Customer::Depoist(Account account)
         }
 }
 
+// function for the customer to make a transfer from the checking to the savings account, or vice versa
 void Customer::Transfer(Account ac1, Account ac2)
 {
         auto it1 = find(this->accounts.begin(), this->accounts.end(), ac1);
@@ -106,22 +124,21 @@ void Customer::Transfer(Account ac1, Account ac2)
                 cout << "Balance in the Account:" << it2->balance << endl;
         }
 }
-//==========================================================================================
-// function to display the main menu
+
+//Methods of the Class ATM
+
+// function to add a new customer to an ATM machine
 void ATM::addCustomer(Customer c)
 {
         this->customers.push_back(c);
 }
 
 // function to validate the PIN of a customer using the ATM
-bool ATM::validatePIN()
+bool ATM::validatePIN(string pin)
 {
-        string input_pin;
-        cout << "Please enter your PIN:" << endl;
-        cin >> input_pin;
         for(auto customer:this->customers)
         {
-                if(input_pin == customer.PIN)
+                if(customer.PIN == pin)
                 {
                         return true;
                         break;
@@ -130,7 +147,8 @@ bool ATM::validatePIN()
         return false;
 }
 
-void ATM::displayMenu()
+// function to display the ATM's main menu
+void ATM::displayMenu(Customer c)
 {
         int option;
 	cout << "----Main Menu----" <<endl;
@@ -138,11 +156,12 @@ void ATM::displayMenu()
 	cout << "\t1. Checkings \n\t2. Savings \n\t3. Exit" << endl;
 	cin >> option;
         char response;
+	auto it = this->customers.find(c);
 	switch(option)
         {
-		case 1: this->displayFunction(option); // checking account
+		case 1: this->displayFunction(c, option); // checking account
 			break;
-		case 2: this->displayFunction(option); // savings account
+		case 2: this->displayFunction(c, option); // savings account
 			break;
 		default:
 			cout << "Would you like to continue (y/n)\n";
@@ -160,7 +179,88 @@ void ATM::displayMenu()
 	}
 }
 
-void ATM::displayFunction()
+// function to display action selection for a cunstomer
+void ATM::displayFunction(Customer c, int option)
 {
-        
+        // account option = 1 (checkings)
+	// account option = 2 (savings)
+	cout << "Please select your action for your " <<AccountType[option] << "account:" << endl;
+	cout << "--\n\t1. Check balance"
+		<<"\n\t2. Withdraw from " << AccountType[option] 
+		<<"\n\t3. Deposit to " << AccountType[option] 
+		<<"\n\t4. Transfer " 
+		<<"\n\t5. --Return to Menu." << endl;
+
+	// Pass in account type
+	Account accout_process, account_transfer;
+	for(auto account:c.accounts) // we assume one customer can set up at most two accounts (one checking and one savings)
+	{
+		if(accout.type == option)
+		{
+			account_process = account;
+		}
+		else
+		{
+			account_transfer = account;
+		}			
+	}
+
+	int selectMenu;
+	cin >> selectMenu;
+	string response;
+	switch(selectMenu)
+	{
+		case 1:
+			cout << accout_process.getBalance();
+			break;
+		case 2: 
+			cout << c.Withdraw();
+			break;
+		case 3: 
+			cout << c.Deposit();
+			break;
+		case 4:
+			cout << c.Transfer(account_process, account_transfer);
+			break;
+		case 5:
+			this->displayMenu(c); // return to main menu
+			break;
+		default:
+			cout << "Would you like to continue (y/n)\n";
+			cin >> response;
+			if(response == 'y' || response == 'Y')
+                        {
+                               	this->displayMenu(c);
+                        }
+                        else if(response == 'n' || response == 'N')
+                        {
+                                cout << "Thank you for banking with us." << endl;
+                                exit(0);
+                        }
+			break;
+	}
+}
+
+
+// Test function
+int main() 
+{
+	Customer newCustomer("Cus", "1234");
+	newCustomer.CreateNewAccount(1, 500); // checking account
+	newCustomer.CreateNewAccount(2, 500); // savings account
+	ATM atmMachine;
+	atmMachine.addCustomer(newCustomer);
+	
+	string pin;
+	cout << "Welcome to Bank of Programming.\n\tPlease enter your PIN to access your account:" << endl;
+	do {
+		cin >> pin;
+		if(atmMachine.validatePIN(pin)) {
+			atmMachine.displayMenu(); // continue to main menu
+		} else {
+			cout << "Invalid pin. Please enter pin number again:" << endl; 
+		}
+	} while (!atmMachine.validatePin(pin));
+
+	return 0;
 }
